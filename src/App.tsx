03c2cd1c42f28b8f36cd791cd2462698a75bf0f4
Belen Luna <belen.luna@flat.mx>
2023-12-20 15:28:28 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import "./App.css";
+
+interface Item {
+  id: `${string}-${string}-${string}-${string}-${string}`;
+  timestamp: number;
+  text: string;
+}
+
+const INITIAL_ITEMS: Item[] = [
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: "Videojuegos",
+  },
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: "Libros",
+  },
+];
+
+function App() {
+  const [items, setItems] = useState(INITIAL_ITEMS);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { elements } = event.currentTarget;
+    const input = elements.namedItem("item");
+    const isInput = input instanceof HTMLInputElement;
+    if (!isInput || input == null) return;
+
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      timestamp: Date.now(),
+      text: input.value,
+    };
+
+    setItems((prevItems) => {
+      return [...prevItems, newItem];
+    });
+    input.value = "";
+  };
+  return (
+    <main>
+      <aside>
+        <h1>First evaluation</h1>
+        <h2>Añadir y eliminar elementos de una lista</h2>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Elemento a introducir:
+            <input name="item" required type="text" placeholder="Videojuegos" />
+          </label>
+          <button> Añadir elemento a la lista</button>
+        </form>
+      </aside>
+
+      <section>
+        <h2>Lista de elementos</h2>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.text}
+                <button
+                  onClick={() => {
+                    setItems((prevItems) => {
+                      return prevItems.filter(
+                        (currenItem) => currenItem.id === item.id
+                      );
+                    });
+                  }}
+                >
+                  Eliminar elemento
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default App;
